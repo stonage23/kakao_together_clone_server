3c8d025d177271553735b0808e20d17f544dd39d
stonage23 <stonage23@gmail.com>
2024-06-14 07:42:03 +0900

    프로젝트 최초 커밋

---
+++ kakao_together_clone/src/main/java/com/kakao/together/KakaoTogetherCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.kakaoTogetherClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoTogetherCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoTogetherCloneApplication.class, args);
+	}
+
+}
---
+++ kakao_together_clone/src/test/java/com/kakao/together/KakaoTogetherCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoTogetherCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
