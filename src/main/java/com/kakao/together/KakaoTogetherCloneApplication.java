6b01dd166ad61ccbcbe635c07f056e6fe4c43a42
stonage23 <stonage23@gmail.com>
2025-07-15 23:32:50 +0900

    Feat: [#9] Spring Boot 프로젝트 생성 및 환경설정

---
+++ src/main/java/com/kakao/together/KakaoTogetherCloneApplication.java
@@ -0,0 +1,11 @@
+package com.kakao.together;
+
+import org.springframework.boot.SpringApplication;
+
+public class KakaoTogetherCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoTogetherCloneApplication.class, args);
+	}
+
+}
