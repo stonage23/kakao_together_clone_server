29b3b9376afdb4fdad8b9920cf1cb7d4d2d5e586
stonage23 <stonage23@gmail.com>
2025-07-16 16:55:57 +0900

    Fix: [#9] Application SpringBootApplication 누락 수정

--- src/main/java/com/kakao/together/KakaoTogetherCloneApplication.java
+++ src/main/java/com/kakao/together/KakaoTogetherCloneApplication.java
@@ -1,7 +1,9 @@
 package com.kakao.together;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class KakaoTogetherCloneApplication {
 
 	public static void main(String[] args) {
