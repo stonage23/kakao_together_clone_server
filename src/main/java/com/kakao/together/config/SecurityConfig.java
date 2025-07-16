68696414d29e839bd82854d50bdc2616c4c47ff0
stonage23 <stonage23@gmail.com>
2025-07-16 21:06:52 +0900

    Feat: [#11] SpringSecurityConfig 파일 생성 SpringSecurityConfig파일 생성, 테스트위해 SecurityAutoConfiguration exclude

---
+++ src/main/java/com/kakao/together/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.kakao.together.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                ;
+
+        return http.build();
+    }
+}
