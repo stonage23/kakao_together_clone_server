05a07b47bf3b2e192b6bfaa57be070c4e30b50fc
stonage23 <stonage23@gmail.com>
2025-07-18 23:05:04 +0900

    Feat: [#12] 전역 예외 핸들러 공통 메소드 구현
    파라미터 유효성 검증 예외 처리(MethodArgumentNotValidException,
    ConstraintViolationException)
    커스텀 예외, 예상치 못한 예외 처리 메소드 작성

--- src/main/java/com/kakao/together/exception/ErrorCode.java
+++ src/main/java/com/kakao/together/exception/ErrorCode.java
@@ -12,8 +12,12 @@ public enum ErrorCode {
     TEST_ERROR("TEST_ERROR", "test ErrorCode", HttpStatus.BAD_REQUEST),
 
     INTERNAL_SERVER_ERROR("INTERNAL_SERVER_ERROR", "서버 에러이므로 서버 팀에 연락주세요.", HttpStatus.INTERNAL_SERVER_ERROR),
-    END(null, null, null),
-    ;
+
+    // 유저
+    NOT_FOUND_USER("NOT_FOUND_USER", "유저가 존재하지 않습니다.", HttpStatus.NOT_FOUND),
+    NOT_FOUND_PROFILE("NOT_FOUND_PROFILE", "프로필이 존재하지 않습니다.", HttpStatus.NOT_FOUND),
+    DUPLICATE_EMAIL("DUPLICATE_EMAIL", "중복된 이메일입니다.", HttpStatus.CONFLICT),
+    INVALID_INPUT_VALUES("INVALID_INPUT_VALUES", "입력 정보에 문제가 존재합니다.", HttpStatus.BAD_REQUEST);
 
     private final String code;
     private final String message;
--- src/main/java/com/kakao/together/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kakao/together/exception/GlobalExceptionHandler.java
@@ -1,10 +1,48 @@
 package com.kakao.together.exception;
 
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     private static final String DEFAULT_FILED = "message";
     private final ErrorCode INTERNAL_SERVER_ERROR = ErrorCode.INTERNAL_SERVER_ERROR;
+
+    // @Valid 유효성 검증 실패 케이스
+    @ExceptionHandler({ MethodArgumentNotValidException.class })
+    protected ResponseEntity<ErrorResponse> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.info("##### 전역 예외처리:  MethodArgumentNotValidException");
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUES, e.getBindingResult());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    // @Validated 유효성 검증 실패 케이스
+    @ExceptionHandler({ ConstraintViolationException.class })
+    protected ResponseEntity<ErrorResponse> constraintViolationException(ConstraintViolationException e) {
+        log.info("##### 전역 예외처리 : ConstraintViolationException");
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUES, e.getConstraintViolations());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({ CustomException.class })
+    protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+        log.info("##### 전역 예외처리 : CustomException");
+        ErrorResponse response = ErrorResponse.of(e);
+        return new ResponseEntity<>(response, e.getErrorCode().getHttpStatus());
+    }
+
+    @ExceptionHandler({ Exception.class })
+    protected  ResponseEntity<ErrorResponse> exception(Exception e) {
+        log.info("##### 전역 예외처리 : Exception");
+        log.error("예상치 못한 예외 발생: {}; {}", e.getClass(), e.getMessage());
+        ErrorResponse response = ErrorResponse.of(INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response, INTERNAL_SERVER_ERROR.getHttpStatus());
+    }
 }
