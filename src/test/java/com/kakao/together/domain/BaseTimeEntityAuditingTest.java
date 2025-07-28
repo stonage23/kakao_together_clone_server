4416dd237edd409940b3b69dd02b13498661b53d
stonage23 <stonage23@gmail.com>
2025-07-28 11:48:20 +0900

    Feat: [#10] Jpa Auditing 설정 및 테스트
    테스트 환경에서 test 프로필 전역 사용 build.gradle 설정 추가

---
+++ src/test/java/com/kakao/together/domain/BaseTimeEntityAuditingTest.java
@@ -0,0 +1,68 @@
+package com.kakao.together.domain;
+
+import com.kakao.together.config.JpaAuditingConfig;
+import com.kakao.together.domain.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@Import(JpaAuditingConfig.class)
+@Slf4j
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class BaseTimeEntityAuditingTest {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Entity
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    static class AuditingTestEntity extends BaseTimeEntity {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        private String text;
+    }
+
+    @Test
+    @DisplayName("엔티티 생성 및 업데이트에 Auditing 적용 테스트")
+    public void testAuditing() {
+        // Given
+        AuditingTestEntity entity = new AuditingTestEntity();
+        entity.setText("엔티티생성");
+        entityManager.persist(entity);
+        entityManager.flush();
+        // When
+        AuditingTestEntity savedEntity = entityManager.find(AuditingTestEntity.class, entity.getId());
+        // Then
+        assertThat(savedEntity.getCreatedAt()).isNotNull();
+        assertThat(savedEntity.getUpdatedAt()).isNotNull();
+        assertThat(savedEntity.getUpdatedAt()).isEqualTo(savedEntity.getCreatedAt());
+        log.info(savedEntity.getCreatedAt().toString());
+        log.info(savedEntity.getUpdatedAt().toString());
+        // When
+        savedEntity.setText("엔티티업데이트");
+        entityManager.persist(savedEntity);
+        entityManager.flush();
+        AuditingTestEntity updatedEntity = entityManager.find(AuditingTestEntity.class, savedEntity.getId());
+        // Then
+        assertThat(updatedEntity.getUpdatedAt()).isAfter(updatedEntity.getCreatedAt());
+        log.info(savedEntity.getUpdatedAt().toString());
+    }
+}
